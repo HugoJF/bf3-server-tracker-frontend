e799d7dd227e5309cd15b67a52ab8049b489d009
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-09-06 00:36:34 -0400

    Added remainder of map names

--- src/app/server-card/server-card.component.ts
+++ src/app/server-card/server-card.component.ts
@@ -17,11 +17,14 @@ export class ServerCardComponent implements OnInit {
 
   gametype() {
     const type = this.server.raw.gametype;
-    const types: {[id: string]: string} = {
+    const types: { [id: string]: string } = {
       ConquestSmall0: 'Conquest',
       ConquestLarge0: 'Conquest Large',
+      ConquestSmall1: 'Conquest Assault',
       TeamDeathMatch0: 'Team Deathmatch',
       RushLarge0: 'Rush',
+      SquadRush0: 'Squad Rush',
+      SquadDeathMatch0: 'Squad Deathmatch',
     };
 
     if (type in types) {
@@ -33,12 +36,36 @@ export class ServerCardComponent implements OnInit {
 
   map() {
     const map = this.server.map;
-    const maps: {[id: string]: string} = {
-      MP_012: 'Operation Firestorm',
-      MP_017: 'Noshahr Canals',
+    const maps: { [id: string]: string } = {
       MP_001: 'Grand Bazaar',
+      MP_003: 'Tehran Highway',
       MP_007: 'Caspian Border',
+      MP_011: 'Seine Crossing',
+      MP_012: 'Operation Firestorm',
+      MP_013: 'Damavand Peak',
+      MP_017: 'Noshahr Canals',
+      MP_018: 'Kharg Island',
       MP_Subway: 'Operation Metro',
+      XP1_001: 'Strike at Karkand',
+      XP1_002: 'Gulf of Oman',
+      XP1_003: 'Sharqi Peninsula',
+      XP1_004: 'Wake Island',
+      XP2_Factory: 'Scrapmetal',
+      XP2_Office: 'Operation 925',
+      XP2_Palace: 'Donya Fortress',
+      XP2_Skybar: 'Ziba Tower',
+      XP3_Alborz: 'Alborz Mountains',
+      XP3_Desert: 'Bandar Desert',
+      XP3_Shield: 'Armored Shield',
+      XP3_Valley: 'Death Valley',
+      XP4_FD: 'Markaz Monolith',
+      XP4_Quake: 'Epicenter',
+      XP4_Rubble: 'Talah Market',
+      XP4_Parl: 'Azadi Palace',
+      XP5_001: 'Operation Riverside',
+      XP5_002: 'Nebandan Flats',
+      XP5_003: 'Kiasar Railroad',
+      XP5_004: 'Sabalan Pipeline',
     };
 
     if (map in maps) {
